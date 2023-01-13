bf80ca3036466faf9922c660c233caa33d0e1cbd
Sean <110854028+ExpressOwl@users.noreply.github.com>
2023-01-13 03:55:43 +0000

    Aspect Ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.html','./build/js/*.js'],
+  content: ['./build/**/*.{html,js}'],
   theme: {
     extend: {
       screens: {
         'widescreen': { 'raw': '(min-aspect-ratio: 3/2)'},
-        'tallscreen': { 'raw': '(min-aspect-ratio: 65/100)'},
+        'tallscreen': { 'raw': '(max-aspect-ratio: 65/100)'},
       },
       keyframes: {
         'open-menu': {
